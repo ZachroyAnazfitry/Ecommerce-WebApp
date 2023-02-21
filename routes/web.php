25ac549b36a6646c931341fa08caf615cdbd5377
ZachAnaz <zachroy04@yahoo.com>
2023-02-21 17:09:22 +0800

    setup admin view

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
