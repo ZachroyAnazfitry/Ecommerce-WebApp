59919623a7389f9b6d390a8b435f5712bc02e513
ZachAnaz <zachroy04@yahoo.com>
2023-02-24 11:03:49 +0800

    notification with toastr

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -21,7 +21,7 @@ class AdminController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect('/login')->with('message', "You're logged out!");
     }
 
     // for user profile setting
@@ -46,6 +46,42 @@ class AdminController extends Controller
         $admin = User::find($id);
 
         return view('admin.admin-edit-profile', compact('admin'));
+
+    }
+
+    public function storeProfile(Request $request)
+    {
+        // need id
+        $id = Auth::user()->id;
+        // find who is login
+        $admin = User::find($id);
+
+        $admin->name = $request->name;
+        $admin->username = $request->username;
+        $admin->email = $request->email;
+
+        // for file type image
+        // if ($request->file('image')) {
+        //     $image = $request->file('image');
+
+        //     change image name
+        //     $imageName = date(YMd).$file->getClientOriginalName();
+        //     move file
+        //     $imageName->move(public_path('upload/admin_images', $imageName));  #create new folder to store uploaded images
+        //     $admin['image'] = $imageName;
+        // }
+
+        $admin->save();
+
+        // toastr notifications
+        // $noti = array(
+        //     'message' => "Admin profile updated succesfully",
+        //     'session' => 'success'
+            
+        // );
+
+        return redirect()->route('admin.profile')->with('message', 'Admin profile updated succesfully');
+
 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::controller(AdminController::class)->group(function () {
     Route::get('/admin/logout', 'destroy')->name('admin.logout');
     Route::get('/admin/profile', 'profile')->name('admin.profile');
     Route::get('/admin/profile/edit', 'editProfile')->name('admin.edit');
+    Route::post('/admin/profile/store', 'storeProfile')->name('store.profile');
     // Route::post('/orders', 'store');
 });
 
