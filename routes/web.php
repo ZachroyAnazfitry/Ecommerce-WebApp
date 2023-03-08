943f67c9d230d0ffe2a9cfdd291ac2d37ccf523e
ZachAnaz <zachroy04@yahoo.com>
2023-03-08 12:23:39 +0800

    change homepage

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return view('frontend.main');
-    return view('welcome');
+    return view('frontend.main');
+    // return view('welcome');
 });
 
 // Route::get('/dashboard', function () {
